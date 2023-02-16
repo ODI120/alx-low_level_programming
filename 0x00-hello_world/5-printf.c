4acbaa75abdc6170dbcd4c8ec9fb8e15a53585ad
Odi Chinonye Timothy <oditimothy@gmail.com>
2023-02-16 10:19:24 -0800

    print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the mani function
+ *
+ * Return:  0 when done correctly
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
