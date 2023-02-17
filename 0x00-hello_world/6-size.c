a4eaf5e1bce9ad680a2b06b7c705c1c67f07190e
Odi Chinonye Timothy <oditimothy@gmail.com>
2023-02-17 01:26:39 -0800

    size, git push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,17 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("size of int: %lu byte(s)", sizeof(int));
-	printf("size of a long int: %lu byte(s)", sizeof(long int));
-	printf("size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("size of a float: %lu byte(s)", sizeof(float));
+	int a;
+	char b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+        printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
+        printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+        printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+        printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+	
 	return (0);
 }
