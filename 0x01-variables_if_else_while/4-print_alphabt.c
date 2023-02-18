009489cce05c9151bb7ed01a97e8308295d1ff09
Odi Chinonye Timothy <oditimothy@gmail.com>
2023-02-18 02:29:36 -0800

    ommit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Retur: 0 awlays
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+		return (0);
+}
