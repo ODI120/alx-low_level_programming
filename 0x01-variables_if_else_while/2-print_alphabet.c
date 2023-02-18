32b165f71f522421396305e0e079dcfb54c0858d
Odi Chinonye Timothy <oditimothy@gmail.com>
2023-02-18 02:01:14 -0800

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - main function
  *
@@ -11,6 +12,6 @@ int main(void)
 
 	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
