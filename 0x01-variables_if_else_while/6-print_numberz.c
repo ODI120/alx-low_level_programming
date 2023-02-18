747b4a74574d89219afcdbbd8fb1208b04a90d8c
Odi Chinonye Timothy <oditimothy@gmail.com>
2023-02-18 03:00:38 -0800

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdioi.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return  (0);
+}
