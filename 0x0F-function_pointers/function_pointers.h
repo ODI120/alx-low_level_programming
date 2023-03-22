d402e9433fa3efb01a37c52e0335f972e6c23749
Odi Chinonye Timothy <oditimothy@gmail.com>
2023-03-22 06:38:48 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -10,7 +10,7 @@
 	int _putchar(char c);
 	void array_iterator(int *array, size_t size, void (*action)(int));
 	int int_index(int *array, int size, int (*cmp)(int));
-
+	int (*get_op_func(char *s))(int, int);
 
 	#endif
 
